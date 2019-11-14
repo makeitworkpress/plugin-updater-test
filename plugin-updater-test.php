33053328fc79a5e68ff76b26fb67994f1aa122cb
Michiel <michielsite@hotmail.com>
2019-11-14 12:31:06 +0100

    Updated composer module

--- plugin-updater-test.php
+++ plugin-updater-test.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Plugin Name:  Plugin Updater Test
+ * Plugin URI:   https://www.makeitwork.press/
  * Description:  WordPress Plugin used to test the updater script.
  * Version:      0.1
  * Author:       Make it WorkPress
@@ -14,9 +15,21 @@
 /**
  * Set-up our autoloader from composer
  */
-require_once 'vendor/autoload.php';
+spl_autoload_register( function($classname) {
+
+    $class      = str_replace( '\\', DIRECTORY_SEPARATOR, str_replace( '_', '-', strtolower($classname) ) );
+    $vendor     = str_replace( 'makeitworkpress' . DIRECTORY_SEPARATOR, '', $class );
+    $vendor     = 'makeitworkpress' . DIRECTORY_SEPARATOR . preg_replace( '/\//', '/src/', $vendor, 1 ); // Replace the first slash for the src folder
+    $vendors    = dirname(__FILE__) .  DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . $vendor . '.php';
+    
+    if ( file_exists( $vendors) ) {
+        include_once $vendors;
+    }
+
+} );
+
 
 /**
  * Set-up the updater
  */
-$updater = new MakeitWorkPress\WP_Updater\Boot(['type' => 'plugin', 'source' => 'https://github.com/makeitworkpress/plugin-updater-test.git']);
+new MakeitWorkPress\WP_Updater\Boot(['type' => 'plugin', 'source' => 'https://github.com/makeitworkpress/plugin-updater-test']);
