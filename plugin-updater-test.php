9ef1e6f161f65a7594e540a56353cba1d4031480
Michiel Tramper <michielsite@hotmail.com>
2023-10-19 12:31:25 +0200

    Update plugin-updater-test.php

--- plugin-updater-test.php
+++ plugin-updater-test.php
@@ -15,30 +15,29 @@
 /**
  * Set-up our autoloader from composer
  */
-spl_autoload_register( function($className) {
+spl_autoload_register( function($class_name) {
     
-    $calledClass    = str_replace( '\\', DIRECTORY_SEPARATOR, str_replace( '_', '-', strtolower($className) ) );
-    $parentDir      = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+    $called_class   = str_replace( '\\', '/', str_replace('_', '-', $class_name) );
     
-    // Require main parent classes
-    $parentClass    = $parentDir . 'classes' . DIRECTORY_SEPARATOR . $calledClass . '.php';
-
-    if( file_exists($parentClass) ) {
-        require_once( $parentClass );
+    // Plugin Classes
+    $plugin_spaces  = explode( '/', str_replace( 'plugin-updater-test/', '', $called_class) );
+    $final_class    = array_pop($plugin_spaces);
+    $class_rel_path = $plugin_spaces ? implode('/', $plugin_spaces) . '/class-' . $final_class : 'class-' . $final_class;
+    $class_file     = dirname(__FILE__) .  '/classes/' . $class_rel_path . '.php';
+    
+    if( file_exists($class_file) ) {
+        require_once( $class_file );
         return;
-    } 
-    
-    // Require Vendor (composer) classes
-    $classNames     = explode(DIRECTORY_SEPARATOR, $calledClass);
-    array_splice($classNames, 2, 0, 'src');
-
-    $vendorClass    = $parentDir . 'vendor' . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $classNames) . '.php';
-
-    if( file_exists($vendorClass) ) {
-        require_once( $vendorClass );    
     }
-   
-} );
+
+    // Require Vendor (composer) classes
+    array_splice($plugin_spaces, 2, 0, 'src');
+    $vendor_class_file  = dirname(__FILE__) . '/vendor/' . implode(DIRECTORY_SEPARATOR, $plugin_spaces) . '/' . $final_class . '.php';
+
+    if( file_exists($vendor_class_file) ) {
+        require_once( $vendor_class_file );    
+    }    
+});
 
 /**
  * Set-up the updater
